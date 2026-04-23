f8504b4698114a042890b87667a114f2a4ad007e
Eric de Ridder <eric@ericderidder.nl>
2026-04-23 10:53:29 +0200

    Menu

---
+++ src/scripts/navigation.js
@@ -0,0 +1,27 @@
+const BREAKPOINT = 835;
+
+const originalNav = document.querySelector('.nav-original');
+const inner = originalNav.querySelector('.nav--inner');
+const right = originalNav.querySelector('.right');
+
+let mobileNav = null;
+
+function applyLayout() {
+if (window.innerWidth < BREAKPOINT) {
+    if (!mobileNav) {
+    mobileNav = document.createElement('nav');
+    mobileNav.className = originalNav.className + ' nav-new';
+    originalNav.insertAdjacentElement('afterend', mobileNav);
+    }
+    mobileNav.appendChild(right);
+} else {
+    if (mobileNav) {
+    inner.appendChild(right);
+    mobileNav.remove();
+    mobileNav = null;
+    }
+}
+}
+
+applyLayout();
+window.addEventListener('resize', applyLayout);
